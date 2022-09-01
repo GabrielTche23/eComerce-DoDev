1614d2fc60fc5ac35d5c96f45e5c5af1fb035634
Gabriel Rodrigo <pdktche-pb@live.com>
2022-08-31 22:20:11 -0300

    Foi criado a função de buscar o produto digitando o id do produto .

--- eComerce DoDev.js
+++ eComerce DoDev.js
@@ -2,13 +2,27 @@ var id = []
 var nome = []
 var preco = []
 var avaliacao = []
-var contador
+var contador, comparador, index
 ProdutosCadastrados();
-function ProdutosCadastrados(idParametro,nomeParametro,precoParametro,avaliacaoParametro){
-    id = prompt ("Digite o id dos produtos: ")
-    nome = prompt ("Digite o nome dos produtos: ")
+BuscarProduto();
+function ProdutosCadastrados() {
+    id = prompt("Digite o id dos produtos: ")
+    nome = prompt("Digite o nome dos produtos: ")
     preco = prompt("Digite o preço dos produtos: ")
     avaliacao = prompt("Digite a avaliação dos produtos: ")
     contador++
-    return console.log("ID:",id,"Nome do Produto: ",nome,"Preço do Produto: ",preco,"Avaliação do Produto: ",avaliacao)
+    return console.log("ID:", id, ",", "Nome do Produto: ", nome, ",", "Preço do Produto: ", preco, "Avaliação do Produto: ", avaliacao)
+}
+var buscarId = prompt("Digite o ID do produto que deseja procurar: ")
+
+function BuscarProduto(idParametro) {
+    idParametro = buscarId
+    for (index = 0; index < id.length; index++) {
+        if (idParametro == id[comparador]){
+            console.log(id,nome,preco,avaliacao)
+        }
+        else{
+            comparador++
+        }
+    }
 }
